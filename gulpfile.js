58f7ba6f5d3888fed78cbb78663ce1f25695a367
Hakan Ünlü <unluhak@outlook.com>
2021-10-18 14:26:38 +0200

    Project structur

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return src('index.scss').pipe(sass()).pipe(dest('css'));
+  return src('beruhmt/**/*.scss').pipe(sass()).pipe(dest('css'));
 }
 
 function watchTask() {
-  watch(['index.scss'], buildStyles);
+  watch(['beruhmt/**/*.scss'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
